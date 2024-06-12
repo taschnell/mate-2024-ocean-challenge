f467c31c046030d7d3b0bbb146d8a52eb9e6b817
taschnell <schnellteo@hotmail.com>
2024-06-12 13:00:35 -0700

    some comments and a counter

--- get_images.py
+++ get_images.py
@@ -2,32 +2,40 @@ import json
 import os
 import requests
 
+
 def download_image(url, filename, image_dir="images"):
-  try:
-    os.makedirs(image_dir, exist_ok=True)
+    try:
+        os.makedirs(image_dir, exist_ok=True)
 
-    filepath = os.path.join(image_dir, filename)
+        filepath = os.path.join(image_dir, filename)
 
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
 
-    extension = response.headers.get('Content-Type', '').split('/')[-1]
+        extension = response.headers.get("Content-Type", "").split("/")[-1]
 
-    filename = f"{filename}.{extension}" if extension else f"{filename}.jpg"
+        # These should really all be PNG's
+        filename = f"{filename}.{extension}" if extension else f"{filename}.jpg"
+        
+        with open(filepath, "wb") as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
 
-    with open(filepath, 'wb') as f:
-      for chunk in response.iter_content(1024):
-        f.write(chunk)
+        print(f"Image downloaded: {filepath}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading {url}: {e}")
 
-    print(f"Image downloaded: {filepath}")
-  except requests.exceptions.RequestException as e:
-    print(f"Error downloading {url}: {e}")
 
-with open('data\Ophiuroidea.json', 'r') as f:
-  data = json.load(f)
+with open("data\Ophiuroidea.json", "r") as f:
+    data = json.load(f)
 
-for item in data:
-  image_url = item['url']
-  filename = item['url'].split('/')[-1] 
+# Expect 2687 Images
+if __name__ == "__main__":
+    i = 0
+    for item in data:
+        i += 1
+        image_url = item["url"]
+        filename = item["url"].split("/")[-1]
 
-  download_image(image_url, filename)
+        print(f"{i} | ", end="")
+        download_image(image_url, filename)
